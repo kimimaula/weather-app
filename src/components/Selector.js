13d3090486cdef7c23cb2ecc2ee864c176f67309
Kimmi <kimmi@yoozrr.com>
2020-08-19 15:29:27 +0800

    added custom select component

---
+++ src/components/Selector.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+const CustomSelect = () => {
+  const [selectText, setSelectText] = useState('')
+  const [showOptionList, setShowOptionList] = useState(false)
+  const [optionsList, setOptionsList] = useState([])
+
+  return (
+    <div> test </div>
+  )
+}
+
+export default CustomSelect

e62bc987cc3959495800db4645713544814f4b52
Kimmi <kimmi@yoozrr.com>
2020-08-19 15:31:49 +0800

    added mininmal components for custom selector

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -1,12 +1,28 @@
 import React, { useState } from 'react'
 
 const CustomSelect = () => {
-  const [selectText, setSelectText] = useState('')
+  const [selectedText, setSelectedText] = useState('')
   const [showOptionList, setShowOptionList] = useState(false)
   const [optionsList, setOptionsList] = useState([])
 
   return (
-    <div> test </div>
+    <div >
+      <div
+      >
+        {selectedText}
+      </div>
+      {showOptionList && (
+        <ul className="select-options">
+          {showOptionList.map(option => {
+            return (
+              <li key={option.name}>
+                {option.name}
+              </li>
+            )
+          })}
+        </ul>
+      )}
+    </div>
   )
 }
 

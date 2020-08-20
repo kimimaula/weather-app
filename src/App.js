38c63af104f68727a11dec3e4f090cf5b719e989
Kimmi <kimmi@yoozrr.com>
2020-08-20 09:58:50 +0800

    added list component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Input, notification } from 'antd'
+import { Input, notification, Divider, List, Skeleton, Avatar } from 'antd'
 import config from './components/utils/config'
 import axios from 'axios'
 import './App.css'
@@ -13,49 +13,79 @@ async function getWeather (coords) {
   console.log('fetchWeather', fetchWeather)
 }
 
-async function getLocation (address) {
-  const parsedAddress = encodeURIComponent(address)
-  const fetchLocationData = await axios({
-    method: 'get',
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${parsedAddress}&key=${config.mapkey}`
-  })
-  if (fetchLocationData.data.status.includes('ZERO')) {
-    return notification.warning({
-      message: 'Error',
-      description:
-        'No Results Found'
-    })
-  } else if (fetchLocationData.data.status.includes('OK')) {
-    //add the results to a list
-  } else {
-    return notification.warning({
-      message: 'Error',
-      description:
-        'An Error Occured, please refresh or try again later'
-    })
-  }
-}
-
 function App (props) {
   const initialState = { lat: '', lng: '' }
   const [data, setData] = useState([])
   const [value, setValue] = useState(undefined)
   const [mapPosition, setMapPosition] = useState(initialState)
+  const [loading, setLoading] = useState(false)
   const handleChange = value => {
     const searchValue = value || []
     setValue({ searchValue })
   }
 
+  async function getLocation (address) {
+    setLoading(true)
+    const parsedAddress = encodeURIComponent(address)
+    const fetchLocationData = await axios({
+      method: 'get',
+      url: `https://maps.googleapis.com/maps/api/geocode/json?address=${parsedAddress}&key=${config.mapkey}`
+    })
+    console.log('fetchLocationData', fetchLocationData)
+    if (fetchLocationData.data.status.includes('ZERO')) {
+      notification.warning({
+        message: 'Error',
+        description:
+          'No Results Found'
+      })
+      return setLoading(false)
+    } else if (fetchLocationData.data.status.includes('OK')) {
+      setData(fetchLocationData.data.results)
+      return setLoading(false)
+    } else {
+      notification.warning({
+        message: 'Error',
+        description:
+          'An Error Occured, please refresh or try again later'
+      })
+      return setLoading(false)
+    }
+  }
+
   return (
-    <div style={{ display: 'flex', justifyContent: 'center' }}>
-      <Search
-        placeholder="Input Address"
-        enterButton="Search"
-        size="large"
-        style={{ marginTop: 100, width: '50vw' }}
-        onSearch={value => getLocation(value)}
-      />
-    </div>
+    <>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <Search
+          placeholder="Input Address"
+          enterButton="Search"
+          size="large"
+          style={{ marginTop: 100, marginBottom: 50, width: '50vw' }}
+          onSearch={value => getLocation(value)}
+        />
+      </div>
+      <Divider orientation="middle">Location</Divider>
+      <div>
+        <List
+          className="demo-loadmore-list"
+          loading={loading}
+          itemLayout="horizontal"
+          dataSource={data}
+          style={{ marginTop: 50, marginLeft: 'auto', marginRight: 'auto', width: '50vw' }}
+          renderItem={item => (
+            <List.Item
+              actions={[<a key="get-weather">Get Weather</a>]}
+            >
+              <Skeleton avatar title={false} loading={loading} active>
+                <List.Item.Meta
+                  title={<div>{item.address_components[0]?.long_name}</div>}
+                  description={item.formatted_address}
+                />
+              </Skeleton>
+            </List.Item>
+          )}
+        />
+      </div>
+    </>
   )
 }
 
